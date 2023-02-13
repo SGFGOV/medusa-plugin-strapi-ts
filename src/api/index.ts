a37d5ae9e9f29c705e572360643a96cce7b9ec6d
SGF <SGF@sourcegoodfood.com>
2023-02-13 06:22:16 +0000

    fix: added configModule from getConfigfile

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,10 +3,16 @@ import hooksRouter from "./routes/hooks";
 import contentRouter from "./routes/content";
 import { ConfigModule } from "@medusajs/medusa/dist/types/global";
 import adminRouter from "./routes/strapi-admin";
+import { getConfigFile } from "medusa-core-utils";
 
 export default (app, options, config: ConfigModule) => {
     //  app.use("/strapi", strapiRouter);
     // Authenticated routes
+    if (!config) {
+        /** to support standard @medusajs/medusa */
+        const { configModule } = getConfigFile(app, "medusa-config");
+        config = configModule as ConfigModule;
+    }
     const strapiRouter = Router();
 
     hooksRouter(strapiRouter, options);
